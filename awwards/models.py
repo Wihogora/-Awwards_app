e28edb30a50422a4f73e3f8cf16f75f02dd138be
Wihogora Wivine <wwihogora@gmail.com>
2019-04-01 11:40:15 +0200

    Add models in admin.py

--- awwards/models.py
+++ awwards/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+        username = models.CharField(default='User',max_length=40)
+        profile_pic = models.ImageField(upload_to = "profile/",null=True)
+        bio = models.TextField(default='',blank = True)
+        user_project = models.CharField(max_length =40)
+        user_contact = models.CharField(max_length =40)
+
+
+        def __str__(self):
+            return self.username
+
+        def delete_profile(self):
+            self.delete()
+
+        def save_profile(self):
+            self.save()
+
+        @classmethod
+        def search_profile(cls,search_term):
+            got_profiles = cls.objects.filter(user_project__icontains = search_term)
+            return got_profiles
+
---
+++ awwards/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns=[
+    
+    url('^$',views.index, name='index'),
+    url(r'^profile/', views.profile, name='profile'),
+    url(r'^upload/profile', views.upload_profile, name='upload_profile'),
+
+
+]
+
+if settings.DEBUG:
+	urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
