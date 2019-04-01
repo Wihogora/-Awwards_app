ad3c6f916c78bf9d84a9b720b2af11a929ef3f35
Wihogora Wivine <wwihogora@gmail.com>
2019-04-01 14:50:18 +0200

    Add function profile in views.py

--- awwards/views.py
+++ awwards/views.py
@@ -6,3 +6,18 @@ from django.http  import HttpResponse
 from django.conf import settings
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def index(request):
+    title = 'Awwards'
+    image_posts = Image.objects.all()
+   
+
+    print(image_posts)
+    return render(request, 'index.html', {"title":title,"image_posts":image_posts})
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    profile = Profile.objects.all()
+    
+    return render(request, 'profile.html',{"current_user":current_user,"profile":profile})
