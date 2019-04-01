50729606945d195611dae1ecc5663721eec2f5ad
Wihogora Wivine <wwihogora@gmail.com>
2019-04-01 14:54:33 +0200

    Add function upload_profile in views.py

--- awwards/views.py
+++ awwards/views.py
@@ -21,3 +21,54 @@ def profile(request):
     profile = Profile.objects.all()
     
     return render(request, 'profile.html',{"current_user":current_user,"profile":profile})
+
+@login_required(login_url='/accounts/login/')
+def single_image(request,image_id):
+	image = image.objects.get(id= image_id)
+
+	return render(request, 'profile/single_image.html',{"image":image})
+
+
+def search_results(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_image(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search_image.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search_image.html',{"message":message})
+
+
+@login_required(login_url='/accounts/login/')
+def upload_profile(request):
+    current_user = request.user 
+    title = 'Upload Profile'
+    try:
+        requested_profile = Profile.objects.get(user_id = current_user.id)
+        if request.method == 'POST':
+            form = ProfileUploadForm(request.POST,request.FILES)
+
+            if form.is_valid():
+                requested_profile.profile_pic = form.cleaned_data['profile_pic']
+                requested_profile.bio = form.cleaned_data['bio']
+                requested_profile.username = form.cleaned_data['username']
+                requested_profile.save_profile()
+                return redirect( profile )
+        else:
+            form = ProfileUploadForm()
+    except:
+        if request.method == 'POST':
+            form = ProfileUploadForm(request.POST,request.FILES)
+
+            if form.is_valid():
+                new_profile = Profile(profile_pic = form.cleaned_data['profile_pic'],bio = form.cleaned_data['bio'],username = form.cleaned_data['username'])
+                new_profile.save_profile()
+                return redirect( profile )
+        else:
+            form = ProfileUploadForm()
+
+
+    return render(request,'upload_profile.html',{"title":title,"current_user":current_user,"form":form})
