52171e0e1700c33c42574c7cfd821dbfe6cf65fd
Wihogora Wivine <wwihogora@gmail.com>
2019-04-05 14:15:04 +0200

    Add ProfileTestClass

--- awwards/tests.py
+++ awwards/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Project,Profile,Rates
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.awwward= Profile( )
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.awwward,Profile))
