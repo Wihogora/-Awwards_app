beed5b475b31dd39b7d24e0d236d7d1f48de7a9b
Wihogora Wivine <wwihogora@gmail.com>
2019-04-05 14:17:57 +0200

    Add ProjectTestClass

--- awwards/tests.py
+++ awwards/tests.py
@@ -9,3 +9,18 @@ class ProfileTestClass(TestCase):
     # Testing  instance
     def test_instance(self):
         self.assertTrue(isinstance(self.awwward,Profile))
+
+class ProjectTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.awwward= Project()
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.awwward,Project))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.awwward.save_image()
+        description= Project.objects.all()
+        self.assertTrue(len(description) > 0)
